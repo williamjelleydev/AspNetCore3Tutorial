fea450069bcb9099bdb7a82f488cadfc2a5787f9
William Jelley <williamjelleydevman@gmail.com>
2021-07-16 18:19:50 +1200

    everything lol

--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -33,6 +33,7 @@ namespace Globomantics
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            env.IsEnvironment
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
